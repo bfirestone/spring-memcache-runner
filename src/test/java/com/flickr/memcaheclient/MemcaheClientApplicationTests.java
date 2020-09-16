2ae51b7823bf7398c7c2ee400b5330f3e0343b3f
Ben Firestone <benf@smugmug.com>
2020-09-16 16:17:19 -0700

    updating gitignore

---
+++ src/test/java/com/flickr/memcaheclient/MemcaheClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flickr.memcaheclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemcaheClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
